8888ce471ba4806d2fce075aa52a9b4731c0e4fe
Jay Panchal <jaypanchal982@gmail.com>
2022-06-13 10:40:41 +0530

    inital commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import MenuItem from "./MenuItem";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/MenuItem.js
@@ -0,0 +1,12 @@
+const MenuItem = () => {
+  return (
+    <div className="h-[240px] min-w-[30%] border flex justify-center items-center">
+      <div className="h-[90px] flex flex-col justify-center items-center border py-0 px-[25px]">
+        <h1 className="text-xl">HATS</h1>
+        <span>SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
